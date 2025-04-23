41c8bec152547db4971a85709ac37595fa3b8c86
NeiJunio <neijunio50@gmail.com>
2025-04-22 21:53:33 -0300

    Finalização dos testes de unidades (testes de erros)

--- tests/Validators.Tests/RegisterExpenseValidatorTests.cs
+++ tests/Validators.Tests/RegisterExpenseValidatorTests.cs
@@ -1,5 +1,6 @@
 ﻿using CashFlow.Application.UseCases.Expenses.Register;
-using CashFlow.Communication.Requests;
+using CashFlow.Communication.Enums;
+using CashFlow.Exception;
 using CommonTestUtilities.Requests;
 using Shouldly;
 
@@ -20,5 +21,98 @@ public class RegisterExpenseValidatorTests
         result.IsValid.ShouldBeTrue(); // usando biblioteca Shouldly
         // Assert.True(result.IsValid); // método raiz do .NET
         //result.IsValid.Should().BeTrue(); // usando biblioteca FluentAssertions
+    }
+
+
+    [Theory]
+    [InlineData("")]
+    [InlineData("       ")]
+    [InlineData(null)]
+    public void Error_Title_Empty(string title)
+    {
+        // arrange
+        var validator = new RegisterExpenseValidator();
+        var request = RequestRegisterExpenseJsonBuilder.Build();
+        request.Title = title;
+        
+        // act
+        var result = validator.Validate(request);
+
+        // assert
+        result.IsValid.ShouldBeFalse(); // usando biblioteca Shouldly
+        
+        // Garante que só exista UM erro retornado
+        result.Errors.ShouldHaveSingleItem();
+
+        // E que esse erro seja o do título
+        result.Errors.Single().ErrorMessage.ShouldBe(ResourceErrorMessages.TITLE_REQUIRED);
+    }
+
+
+    [Fact]
+    public void Error_Date_Future()
+    {
+        // arrange
+        var validator = new RegisterExpenseValidator();
+        var request = RequestRegisterExpenseJsonBuilder.Build();
+        request.Date = DateTime.UtcNow.AddDays(1);
+
+        // act
+        var result = validator.Validate(request);
+
+        // assert
+        result.IsValid.ShouldBeFalse(); // usando biblioteca Shouldly
+
+        // Garante que só exista UM erro retornado
+        result.Errors.ShouldHaveSingleItem();
+
+        // E que esse erro seja o do título
+        result.Errors.Single().ErrorMessage.ShouldBe(ResourceErrorMessages.EXPENSES_CANNOT_FOR_THE_FUTURE);
+    }
+
+
+    [Fact]
+    public void Error_Payment_Type_Invalid()
+    {
+        // arrange
+        var validator = new RegisterExpenseValidator();
+        var request = RequestRegisterExpenseJsonBuilder.Build();
+        request.PaymentType = (PaymentType)700;
+
+        // act
+        var result = validator.Validate(request);
+
+        // assert
+        result.IsValid.ShouldBeFalse(); // usando biblioteca Shouldly
+
+        // Garante que só exista UM erro retornado
+        result.Errors.ShouldHaveSingleItem();
+
+        // E que esse erro seja o do título
+        result.Errors.Single().ErrorMessage.ShouldBe(ResourceErrorMessages.PAYMENT_TYPE_INVALID);
+    }
+
+
+    [Theory]
+    [InlineData(0)]
+    [InlineData(-1)]
+    public void Error_Amount_Invalid(decimal amount)
+    {
+        // arrange
+        var validator = new RegisterExpenseValidator();
+        var request = RequestRegisterExpenseJsonBuilder.Build();
+        request.Amount = amount;
+
+        // act
+        var result = validator.Validate(request);
+
+        // assert
+        result.IsValid.ShouldBeFalse(); // usando biblioteca Shouldly
+
+        // Garante que só exista UM erro retornado
+        result.Errors.ShouldHaveSingleItem();
+
+        // E que esse erro seja o do título
+        result.Errors.Single().ErrorMessage.ShouldBe(ResourceErrorMessages.AMOUNT_MUST_BE_GREATER_THAN_ZERO);
     }
 }
